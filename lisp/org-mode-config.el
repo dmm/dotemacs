d1b167e06e7b0ec19fa6f89b88ef2b1d6eea5b57
David Mattli <dmattli@usgs.gov>
2011-01-06 13:33:21 -0600

    Add automatic archiving to org-mode

--- lisp/org-mode-config.el
+++ lisp/org-mode-config.el
@@ -79,3 +79,39 @@
  '(remember-annotation-functions (quote (org-remember-annotation)))
  '(remember-handler-functions (quote (org-remember-handler))))
 
+;; Auto archive old tasks
+(defvar org-my-archive-expiry-days 7
+  "The number of days after which a completed task should be auto-archived.
+This can be 0 for immediate, or a floating point value.")
+
+(defun org-my-archive-done-tasks ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((done-regexp
+           (concat "\\* \\(" (regexp-opt org-done-keywords) "\\) "))
+          (state-regexp
+           (concat "- State \"\\(" (regexp-opt org-done-keywords)
+                   "\\)\"\\s-*\\[\\([^]\n]+\\)\\]")))
+      (while (re-search-forward done-regexp nil t)
+        (let ((end (save-excursion
+                     (outline-next-heading)
+                     (point)))
+              begin)
+          (goto-char (line-beginning-position))
+          (setq begin (point))
+          (if (re-search-forward state-regexp end t)
+              (let* ((time-string (match-string 2))
+                     (when-closed (org-parse-time-string time-string)))
+                (if (>= (time-to-number-of-days
+                         (time-subtract (current-time)
+                                        (apply #'encode-time when-closed)))
+                        org-my-archive-expiry-days)
+                    (org-archive-subtree)))
+            (goto-char end)))))
+    (save-buffer)))
+
+(setq safe-local-variable-values (quote ((after-save-hook archive-done-tasks))))
+
+(defalias 'archive-done-tasks 'org-my-archive-done-tasks)
+
