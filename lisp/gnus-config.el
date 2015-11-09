6a153627810cdf214b78705f44e594f2fbfd3fea
David Mattli <dmm@mattli.us>
2015-11-09 04:30:13 -0600

    gnus: Update imap settings.

--- lisp/gnus-config.el
+++ lisp/gnus-config.el
@@ -8,12 +8,12 @@
 (setq gnus-select-method
       '(nnimap "mattli.us"
 		(nnimap-address "mattlimech.com")
-		(nnimap-server-port 993)
+		(nnimap-server-port 143)
 ;		(remove-prefix "INBOX.")
 ;		(nnimap-authinfo-file "/home/dmm/.imap-authinfo")
-		(nnimap-stream tls)
-		))
-
+		(nnimap-stream starttls)
+		)
+)
 (setq nnimap-expunge-on-close "never")
 (setq gnus-permanently-visible-groups "I") ;Always display subscribed groups with e in name ;)
 
