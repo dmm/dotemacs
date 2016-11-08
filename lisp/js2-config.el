0236b084ee8d61acc320b0969b600f8a5287e90d
David Matthew Mattli <dmm@mattli.us>
2016-11-08 16:28:01 -0600

    js2-mode: set indent to 2

--- lisp/js2-config.el
+++ lisp/js2-config.el
@@ -1,2 +1,6 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(setq-default indent-tabs-mode nil)
+;(setq-default tab-width 2)
+(setq-default js2-basic-offset 2)
