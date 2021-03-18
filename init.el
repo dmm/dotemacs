31a7cf135af94e623d1db0f74fb1ebe41527ff26
David Matthew Mattli <dmm@mattli.us>
2021-03-18 16:06:55 -0500

    fix: Update packages...

--- init.el
+++ init.el
@@ -4,8 +4,12 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(eval-when-compile
+  (require 'use-package))
+
 ;; Get SSH_AUTH_SOCK from environment
 (exec-path-from-shell-copy-env "SSH_AUTH_SOCK")
+(exec-path-from-shell-copy-env "PATH")
 
 ;; Some helpful functions
 
@@ -17,31 +21,31 @@
   ;;  "/usr/local/share/emacs/21.0.100/lisp/"
   (interactive "DDirectory name: ")
   (let (el-files-list
-	(current-directory-list
-	 (directory-files-and-attributes directory t)))
+	      (current-directory-list
+	       (directory-files-and-attributes directory t)))
     ;; while we are in the current directory
     (while current-directory-list
       (cond
        ;; check to see whether filename ends in `.el'
        ;; and if so, append its name to a list.
        ((equal ".el" (substring (car (car current-directory-list)) -3))
-	(setq el-files-list
-	      (cons (car (car current-directory-list)) el-files-list)))
-       ;; check whether filename is that of a directory
+	      (setq el-files-list
+	            (cons (car (car current-directory-list)) el-files-list)))
+       ;; check whether filename is that of a directoryx
        ((eq t (car (cdr (car current-directory-list))))
-	;; decide whether to skip or recurse
-	(if
-	    (equal "."
-		   (substring (car (car current-directory-list)) -1))
-	    ;; then do nothing since filename is that of
-	    ;;   current directory or parent, "." or ".."
-	    ()
-	  ;; else descend into the directory and repeat the process
-	  (setq el-files-list
-		(append
-		 (files-in-below-directory
-		  (car (car current-directory-list)))
-		 el-files-list)))))
+	      ;; decide whether to skip or recurse
+	      (if
+	          (equal "."
+		               (substring (car (car current-directory-list)) -1))
+	          ;; then do nothing since filename is that of
+	          ;;   current directory or parent, "." or ".."
+	          ()
+	        ;; else descend into the directory and repeat the process
+	        (setq el-files-list
+		            (append
+		             (files-in-below-directory
+		              (car (car current-directory-list)))
+		             el-files-list)))))
       ;; move to the next filename in the list; this also
       ;; shortens the list so the while loop eventually comes to an end
       (setq current-directory-list (cdr current-directory-list)))
@@ -90,11 +94,9 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
-   (quote
-    ("d8dc153c58354d612b2576fea87fe676a3a5d43bcc71170c62ddde4a1ad9e1fb" default)))
+   '("d8dc153c58354d612b2576fea87fe676a3a5d43bcc71170c62ddde4a1ad9e1fb" default))
  '(org-agenda-custom-commands
-   (quote
-    (("d" todo "DELEGATED" nil)
+   '(("d" todo "DELEGATED" nil)
      ("c" todo "DONE|DEFERRED|CANCELLED" nil)
      ("w" todo "WAITING" nil)
      ("W" agenda ""
@@ -102,22 +104,16 @@
      ("A" agenda ""
       ((org-agenda-skip-function
         (lambda nil
-          (org-agenda-skip-entry-if
-           (quote notregexp)
-           "\\=.*\\[#A\\]")))
+          (org-agenda-skip-entry-if 'notregexp "\\=.*\\[#A\\]")))
        (org-agenda-ndays 1)
        (org-agenda-overriding-header "Today's Priority #A tasks: ")))
      ("u" alltodo ""
       ((org-agenda-skip-function
         (lambda nil
-          (org-agenda-skip-entry-if
-           (quote scheduled)
-           (quote deadline)
-           (quote regexp)
-           "<[^>
+          (org-agenda-skip-entry-if 'scheduled 'deadline 'regexp "<[^>
 ]+>")))
-       (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
- '(org-agenda-files (quote ("~/todo.org")))
+       (org-agenda-overriding-header "Unscheduled TODO entries: ")))))
+ '(org-agenda-files '("~/todo.org"))
  '(org-agenda-include-diary t)
  '(org-agenda-ndays 7)
  '(org-agenda-show-all-dates t)
@@ -126,19 +122,17 @@
  '(org-agenda-start-on-weekday nil)
  '(org-deadline-warning-days 14)
  '(org-default-notes-file "~/notes.org")
- '(org-fast-tag-selection-single-key (quote expert))
+ '(org-fast-tag-selection-single-key 'expert)
  '(org-remember-store-without-prompt t)
  '(org-remember-templates
-   (quote
-    ((116 "* TODO %?
+   '((116 "* TODO %?
   %u" "~/todo.org" "Tasks")
-     (110 "* %u %?" "~/notes.org" "Notes"))))
+     (110 "* %u %?" "~/notes.org" "Notes")))
  '(org-reverse-note-order t)
  '(package-selected-packages
-   (quote
-    (org2blog add-node-modules-path prettier typescript-mode dockerfile-mode magit poly-ansible jinja2-mode polymode yaml-mode ansible company-lsp lsp-mode cargo rust-mode exec-path-from-shell tide buttercup flycheck-rust)))
- '(remember-annotation-functions (quote (org-remember-annotation)))
- '(remember-handler-functions (quote (org-remember-handler))))
+   '(dash use-package rustic org2blog add-node-modules-path prettier typescript-mode dockerfile-mode magit poly-ansible jinja2-mode polymode yaml-mode ansible company-lsp lsp-mode cargo rust-mode exec-path-from-shell tide buttercup flycheck-rust))
+ '(remember-annotation-functions '(org-remember-annotation))
+ '(remember-handler-functions '(org-remember-handler)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/rust-config.el
+++ lisp/rust-config.el
@@ -1,10 +1,61 @@
-(require 'rust-mode)
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+;(require 'rust-mode)
+;(autoload 'rust-mode "rust-mode" nil t)
+;(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 
-(setq rust-format-on-save t)
+;(setq rust-format-on-save t)
 
+;(with-eval-after-load 'rust-mode
+;  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+;(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+(use-package rustic
+  :ensure t
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm
+  (setq-local buffer-save-without-query t))
+
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
+(use-package flycheck :ensure)
 (with-eval-after-load 'rust-mode
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
-(add-hook 'rust-mode-hook 'cargo-minor-mode)
+;;; rust-config.el ends here
+
