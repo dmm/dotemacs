e115b2b3f5e93a21046b1de142ccdfb70fe5ff74
David Matthew Mattli <davidmattli@bigbadwolfsecurity.com>
2022-08-17 13:41:30 -0500

    Upgrade packages

---
+++ elpa/org2blog-20220811.51/org2blog-pkg.el
@@ -0,0 +1,15 @@
+(define-package "org2blog" "20220811.51" "Blog from Org mode to WordPress"
+  '((htmlize "1.54")
+    (hydra "0.15.0")
+    (xml-rpc "1.6.12")
+    (metaweblog "1.1.1"))
+  :commit "6d99c6b4130b2bc489d9f407ea57149eeb169e4c" :authors
+  '(("Puneeth Chaganti" . "punchagan+org2blog@gmail.com"))
+  :maintainer
+  '("Grant Rettke" . "grant@wisdomandwonder.com")
+  :keywords
+  '("comm" "convenience" "outlines" "wp")
+  :url "https://github.com/org2blog/org2blog")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/vagrant-20220730.302/vagrant-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from vagrant.el  -*- no-byte-compile: t -*-
+(define-package "vagrant" "20220730.302" "Manage a vagrant box from emacs" 'nil :commit "eb4ec2053955eda1ac9e5ff92ded88f1919e13f2" :authors '(("Robert Crim" . "rob@servermilk.com")) :maintainer '("Robert Crim" . "rob@servermilk.com") :keywords '("vagrant" "chef") :url "https://github.com/ottbot/vagrant.el")
