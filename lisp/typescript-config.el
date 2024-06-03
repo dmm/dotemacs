46b0a019dacfa2f484e1350e09060af6a70ced37
David Matthew Mattli <dmm@mattli.us>
2024-06-03 14:16:48 -0500

    Move tide mode config to tide.el

--- lisp/typescript-config.el
+++ lisp/typescript-config.el
@@ -1,3 +1,6 @@
+
+(setq typescript-indent-level 2)
+
 (defun setup-tide-mode ()
   (interactive)
   (tide-setup)
@@ -5,6 +8,7 @@
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
+  (prettier-js-mode)
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
@@ -14,9 +18,10 @@
 (setq company-tooltip-align-annotations t)
 
 ;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
+;(add-hook 'before-save-hook 'tide-format-before-save)
+;(add-hook 'before-save-hook 'prettier-prettify)
 ;; if you use treesitter based typescript-ts-mode (emacs 29+)
 (add-hook 'typescript-ts-mode-hook #'setup-tide-mode)
 
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+
