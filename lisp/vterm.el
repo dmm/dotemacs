627d972739c1df1bbbc2909cdffd3c770448e148
David Matthew Mattli <dmm@framey.lan>
2024-07-14 05:19:26 -0500

    Add new vterm config. Now it cycles through names!

--- lisp/vterm.el
+++ lisp/vterm.el
@@ -16,12 +16,38 @@
   :hook
   (vterm-mode . goto-address-mode))
 
-(defun create-named-vterm (name)
-  "Create a new vterm buffer named *name-vterm*. If the buffer already exists, switch to it."
-  (interactive "sEnter name for vterm buffer: ")
+(defun get-vterm-buffer-clean-names ()
+  "Get a list of all vterm buffer names, stripped of *...-vterm* format."
+  (seq-uniq
+   (mapcar (lambda (name)
+             (replace-regexp-in-string "^\\*\\(.+\\)-vterm\\*$" "\\1" name))
+           (seq-filter (lambda (name) (string-match-p "^\\*.+-vterm\\*$" name))
+                       (mapcar #'buffer-name (buffer-list))))))
+
+(defun create-or-switch-vterm (name)
+  "Create a new vterm buffer named *name-vterm* or switch to it if it exists."
   (let ((buffer-name (format "*%s-vterm*" name)))
     (if (get-buffer buffer-name)
         (switch-to-buffer buffer-name)
       (vterm buffer-name))))
 
-(global-set-key (kbd "C-c v") 'create-named-vterm)
+(defun create-or-cycle-vterm ()
+  "Create a new named vterm buffer or cycle through existing ones.
+Requires a non-empty, non-whitespace name for new buffers."
+  (interactive)
+  (let* ((vterm-clean-names (get-vterm-buffer-clean-names))
+         (name (completing-read "Vterm name: "
+                                (lambda (string pred action)
+                                  (if (eq action 'metadata)
+                                      '(metadata (category . vterm-buffer))
+                                    (complete-with-action action vterm-clean-names string pred)))
+                                (lambda (name)
+                                  (or (member name vterm-clean-names)
+                                      (and (not (string-empty-p name))
+                                           (string-match-p "\\S-" name))))
+                                nil nil 'vterm-buffer-history)))
+    (if (string-empty-p (string-trim name))
+        (message "Please provide name for the vterm buffer.")
+      (create-or-switch-vterm name))))
+
+(global-set-key (kbd "C-c v") 'create-or-cycle-vterm)
