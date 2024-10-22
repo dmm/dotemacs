fdd11bbec4548fe48944dd2c08ee1dd237292bcf
David Matthew Mattli <dmm@framey.lan>
2024-10-22 11:34:18 -0500

    Upgrade packages

---
+++ elpa/ansible-20240926.2144/ansible-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ansible" "20240926.2144"
+  "Ansible minor mode."
+  '((s     "1.9.0")
+    (f     "0.16.2")
+    (emacs "25.1"))
+  :url "https://gitlab.com/emacs-ansible/emacs-ansible"
+  :commit "e171dacc12eeaba5dbd7fd887f94d11b357fe958"
+  :revdesc "e171dacc12ee"
+  :authors '((nil . "k1lowxb[at]gmail[dot]com")
+             (nil . "k1low[at]101000lab[dot]org"))
+  :maintainers '((nil . "k1lowxb[at]gmail[dot]com")
+                 (nil . "k1low[at]101000lab[dot]org")))
---
+++ elpa/gptel-20241018.1935/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20241018.1935"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.4.0")
+    (compat    "29.1.4.1"))
+  :url "https://github.com/karthink/gptel"
+  :commit "05122e2d6deacff40b2ebc0fe8b43aa644f3d712"
+  :revdesc "05122e2d6dea"
+  :keywords '("convenience")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
