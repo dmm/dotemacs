09fcfcbd4fde66b30033517652c7f9607bd2245f
David Matthew Mattli <dmm@framey.lan>
2024-10-25 08:14:12 -0500

    Upgrade packages

---
+++ elpa/gptel-20241023.315/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20241023.315"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.4.0")
+    (compat    "29.1.4.1"))
+  :url "https://github.com/karthink/gptel"
+  :commit "73bcc682f58cbef28fea27e055cc180b801b6a9d"
+  :revdesc "73bcc682f58c"
+  :keywords '("convenience")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
