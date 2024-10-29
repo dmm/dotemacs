7dc15f3627e76a10e202600e468bd0293e39816e
David Matthew Mattli <dmm@framey.lan>
2024-10-29 06:05:56 -0500

    Switch to prettier mode

--- lisp/typescript-config.el
+++ lisp/typescript-config.el
@@ -8,7 +8,7 @@
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
-  (prettier-js-mode)
+  (prettier-mode)
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
