c38d85ab9017fccaba2f0f949af1dbc9dbd97883
David Matthew Mattli <dmm@framey.lan>
2024-12-16 13:35:37 -0600

    gptel: switch to new sonnet 3.5

--- lisp/gptel.el
+++ lisp/gptel.el
@@ -1,10 +1,6 @@
 ;;; gptel.el --- Interact with ChatGPT or other LLMs     -*- lexical-binding: t; -*-
 ;; -*- lexical-binding: t -*-
 
-;;; Commentary:
-
-;;; Code:
-
 (defun decrypt-gpg-api-key (keyname)
   "Return a lambda that decrypts and returns the API key from a file
 identified with KEYNAME. The key is only decrypted once and then
@@ -20,7 +16,26 @@ cached for subsequent calls."
                   (insert-file-contents-literally keyfile)
                   (epg-decrypt-string context (buffer-string))))))
       cached-key)))
-(setq
- gptel-model "claude-3-5-sonnet-20240620"
- gptel-backend (gptel-make-anthropic "Claude"
-                 :stream t :key (decrypt-gpg-api-key "claude")))
+;(setq
+; gptel-model 'claude-3-5-sonnet-20241022
+; gptel-backend (gptel-make-anthropic "Claude"
+;                 :stream t :key (decrypt-gpg-api-key "claude")))
+
+(use-package gptel
+  ;; :straight (:local-repo "~/.local/share/git/gptel/")
+  :commands (gptel gptel-send)
+  :hook ((eshell-mode . my/gptel-eshell-keys))
+  :bind (("C-c C-<return>" . gptel-menu)
+         ("C-c <return>" . gptel-send)
+         :map gptel-mode-map
+         ("C-c C-x t" . gptel-set-topic))
+  :config
+  (setq gptel-default-mode 'org-mode)
+  (setq gptel-model 'claude-3-5-sonnet-20241022)
+  (setq gptel-backend (gptel-make-anthropic "Claude"
+                        :stream t :key (decrypt-gpg-api-key "claude"))))
+
+
+
+
+
