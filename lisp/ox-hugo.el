b3f793d24b8a89213495288ba840fd774d5ab386
David Matthew Mattli <dmm@mattli.us>
2025-02-12 15:39:15 -0600

    Remove old org-mode config

--- lisp/ox-hugo.el
+++
@@ -1,4 +0,0 @@
-(use-package ox-hugo
-  :ensure t   ;Auto-install the package from Melpa
-  :pin melpa  ;`package-archives' should already have ("melpa" . "https://melpa.org/packages/")
-  :after ox)
