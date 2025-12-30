376730e59a5fcb47a9ded766065745e45733afb6
David Matthew Mattli <dmm@mattli.us>
2025-12-30 07:16:30 -0600

    Add claude-code integration

--- init.el
+++ init.el
@@ -157,15 +157,15 @@
  '(org-reverse-note-order t)
  '(package-selected-packages
    '(## add-node-modules-path aidermacs ansible anti-zenburn-theme
-        buttercup cargo-mode command-log-mode company-lsp consult dash
-        dracula-theme eat ef-themes eglot embark embark-consult
-        exec-path-from-shell flycheck-inline flycheck-rust gptel
-        hc-zenburn-theme indent-tools jinja2-mode jtsx lsp-java
-        lsp-javacomp lsp-ui magit magit-lfs marginalia modus-themes
-        org ox-hugo ox-rss phoenix-dark-pink-theme poly-ansible
-        polymode prettier quelpa quelpa-use-package rg rust-mode
-        smart-shift switch-window tide vagrant-tramp vterm yaml-mode
-        yasnippet zenburn-theme))
+        buttercup cargo-mode claude-code command-log-mode company-lsp
+        consult dash dracula-theme eat ef-themes eglot embark
+        embark-consult exec-path-from-shell flycheck-inline
+        flycheck-rust gptel hc-zenburn-theme indent-tools jinja2-mode
+        jtsx lsp-java lsp-javacomp lsp-ui magit magit-lfs marginalia
+        modus-themes org ox-hugo ox-rss phoenix-dark-pink-theme
+        poly-ansible polymode prettier quelpa quelpa-use-package rg
+        rust-mode smart-shift switch-window tide vagrant-tramp vterm
+        yaml-mode yasnippet zenburn-theme))
  '(package-vc-selected-packages
    '((aidermacs :url "https://github.com/MatthewZMD/aidermacs.git")))
  '(remember-annotation-functions '(org-remember-annotation))
