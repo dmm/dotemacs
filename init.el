f9837542366f5871f29a35f24003e300ca1fde47
David Matthew Mattli <dmm@mattli.us>
2026-07-14 06:51:15 -0500

    Add agent-shell

--- init.el
+++ init.el
@@ -157,13 +157,13 @@
      (110 "* %u %?" "~/notes.org" "Notes")))
  '(org-reverse-note-order t)
  '(package-selected-packages
-   '(## add-node-modules-path aidermacs ansible anti-zenburn-theme
-        buttercup cargo-mode claude-code command-log-mode company-lsp
-        consult dash dracula-theme eat ef-themes eglot embark
-        embark-consult exec-path-from-shell flycheck-inline
-        flycheck-rust ghostel gptel hc-zenburn-theme indent-tools
-        jinja2-mode jtsx lsp-java lsp-javacomp lsp-ui magit magit-lfs
-        marginalia modus-themes org ox-hugo ox-rss
+   '(## add-node-modules-path agent-shell aidermacs ansible
+        anti-zenburn-theme buttercup cargo-mode claude-code
+        command-log-mode company-lsp consult dash dracula-theme eat
+        ef-themes eglot embark embark-consult exec-path-from-shell
+        flycheck-inline flycheck-rust ghostel gptel hc-zenburn-theme
+        indent-tools jinja2-mode jtsx lsp-java lsp-javacomp lsp-ui
+        magit magit-lfs marginalia modus-themes org ox-hugo ox-rss
         phoenix-dark-pink-theme poly-ansible polymode prettier quelpa
         quelpa-use-package rg rust-mode smart-shift switch-window tide
         vagrant-tramp vterm yaml-mode yasnippet zenburn-theme))
