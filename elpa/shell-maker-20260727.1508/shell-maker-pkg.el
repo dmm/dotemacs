ff74501490c8482f9e63978d5f5a2a34f76930ab
David Matthew Mattli <dmm@mattli.us>
2026-07-30 19:02:58 -0500

    Upgrade shell-maker

--- elpa/shell-maker-20260727.1508/shell-maker-pkg.el
+++ elpa/shell-maker-20260727.1508/shell-maker-pkg.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "shell-maker" "20260713.844"
+(define-package "shell-maker" "20260727.1508"
   "Interaction mode for making comint shells."
   '((emacs "27.1"))
   :url "https://github.com/xenodium/shell-maker"
-  :commit "071c6df3ca22a2f4c0daa689848ac9bd21bf4e2b"
-  :revdesc "071c6df3ca22")
+  :commit "679cfbc02e206e0a702048cfd7c663eb5c9d1059"
+  :revdesc "679cfbc02e20")
