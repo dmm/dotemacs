1bf6c8e5a3f20318eb8279f50ab3575f2fa011d0
David Matthew Mattli <dmm@mattli.us>
2026-08-31 19:18:26 +0000

    agent-shell: enable verbose restore

--- init.el
+++ init.el
@@ -75,6 +75,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(agent-shell-session-restore-verbosity 'full)
  '(auth-source-save-behavior nil)
  '(c-basic-offset 2)
  '(column-number-mode t)
